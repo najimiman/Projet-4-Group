b0fb2a919018a96b8b7b7b611760dc09e5838156
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-01-04 14:11:17 +0100

    save all crud et message

--- Realisation/API/resources/views/layouts/sidebar.blade.php
+++ Realisation/API/resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
+
   {{-- <a href="../../index3.html" class="brand-link">
     <img src="{{asset('assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
@@ -8,6 +9,7 @@
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user (optional) -->
+
     {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
         <img src= "{{asset('assets/img/user1-128x128.jpg')}}" class="img-circle elevation-2" alt="User Image">
@@ -21,21 +23,47 @@
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
+
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="info">
-          <a href="/" class="d-block">{{__('message.btn_taskManag')}}</a>
-        </div>
+          <a href="/" class="nav-link active m">
+            <i class="nav-icon fas fa-table"></i> &nbsp;
+            <p>
+              {{__('message.btn_taskManag')}}
+            </p>
+          </a>
       </div>
+
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="info">
-          <a href="{{route('apprenant.index')}}" class="d-block">{{__('message.btn_apprenant')}}</a>
-        </div>
+          <a href="{{route('apprenant.index')}}" class="nav-link active m">
+            <i class="nav-icon fas fa-table"></i> &nbsp;
+            <p>
+              {{__('message.btn_apprenant')}}
+            </p>
+          </a>
       </div>
+
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="info">
-          <a href="{{route('assign.index')}}" class="d-block">{{__('message.btn_assign')}}</a>
-        </div>
+          <a href="{{route('brief.index')}}" class="nav-link active m">
+            <i class="nav-icon fas fa-table"></i> &nbsp;
+            <p>
+              {{__('message.titleBrief')}}
+            </p>
+          </a>
       </div>
+
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <a href="{{route('assign.index')}}" class="nav-link active m">
+            <i class="nav-icon fas fa-table"></i> &nbsp;
+            <p>
+              {{__('message.btn_assign')}}
+            </p>
+          </a>
+      </div>
+
+
+    </ul>
       
     </nav>
     <!-- /.sidebar-menu -->
